df1a7c91a0402304411cde75901332aa339f8990
mikejmarin25 <105763252+mikejmarin25@users.noreply.github.com>
2022-06-25 18:22:16 -0400

    changed to js file

--- Develop/script.js
+++ Develop/script.js
@@ -10,18 +10,5 @@ function writePassword() {
 
 }
 
-function myFunction(){
-    let text;
-    let person = prompt("Please enter your name:", "Harry Potter");
-    if (person == null || person == "") {
-      text = "User cancelled the prompt.";
-    } else {
-      text = "Hello " + person + "! How are you today?";
-    }
-    document.getElementById("demo").innerHTML = text;
-  }
-
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
