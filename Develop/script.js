d6c95f2230d515833bcd299e8cac1476e5aff77c
mikejmarin25 <105763252+mikejmarin25@users.noreply.github.com>
2022-06-28 22:30:38 -0400

    added password length funtionality

--- Develop/script.js
+++ Develop/script.js
@@ -2,16 +2,26 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
+
+function writePassword() {
+  var passwordLength = prompt ("Please enter a password between 8-128 characters");
+    if (passwordLength < 8 || passwordLength > 128) {
+      return alert ("Incorrect password length!");
+      passwordLength = null;
+    }
+
+    console.log("The password length is: " + passwordLength);
+    alert ("The password length is set to: " + passwordLength);
+
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
