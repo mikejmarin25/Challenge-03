bd022b6955e62cb65a00a365b9184ae327b9c84c
mikejmarin25 <105763252+mikejmarin25@users.noreply.github.com>
2022-06-28 22:56:41 -0400

    added special chars funtionality

--- Develop/script.js
+++ Develop/script.js
@@ -11,14 +11,30 @@
 //   passwordText.value = password;
 
 function writePassword() {
+  var chars = "";
+  console.log("Character string is " + chars);
+  //Checks for password length
   var passwordLength = prompt ("Please enter a password between 8-128 characters");
     if (passwordLength < 8 || passwordLength > 128) {
       return alert ("Incorrect password length!");
       passwordLength = null;
     }
-
     console.log("The password length is: " + passwordLength);
     alert ("The password length is set to: " + passwordLength);
+  //Checks if the user wants to add special characters
+  var special = "";
+  var s1 = prompt ("Would like to include special characters in this lines?", "yes or no");
+  if (s1 == "yes"){
+    special = "!@#$%^&*()";
+  }
+  if (s1 == "no"){ 
+    special = "";
+  }
+  chars = special;
+  console.log("Character string is " + chars);
+
+
+
 
 
 }
