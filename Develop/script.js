86f4931bd86d2fc164ffab7183fba048bfc0f62a
mikejmarin25 <105763252+mikejmarin25@users.noreply.github.com>
2022-06-28 23:19:05 -0400

    added upper and lower case funtionality

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,5 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 //var generateBtn = document.querySelector("#generate");
 
@@ -11,32 +10,68 @@
 //   passwordText.value = password;
 
 function writePassword() {
-  var chars = "";
+  var chars = ""; //Final character selection for password generator
   console.log("Character string is " + chars);
+  
   //Checks for password length
-  var passwordLength = prompt ("Please enter a password between 8-128 characters");
-    if (passwordLength < 8 || passwordLength > 128) {
-      return alert ("Incorrect password length!");
-      passwordLength = null;
-    }
-    console.log("The password length is: " + passwordLength);
-    alert ("The password length is set to: " + passwordLength);
+  var passwordLength = prompt(
+    "Please enter a password between 8-128 characters"
+  );
+  if (passwordLength < 8 || passwordLength > 128) {
+    return alert("Incorrect password length!");
+    passwordLength = null;
+  }
+  console.log("The password length is: " + passwordLength);
+  alert("The password length is set to: " + passwordLength);
+  
+  // Checks for lower
+  var lower = "";
+  var l1 = prompt("Would like to include lower-case letters", "yes or no");
+  if (l1 == "yes") {
+    lower = "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (l1 == "no") {
+    lower = "";
+  }
+  chars = lower;
+  console.log("Character string is " + chars);
+  
+  //Checks for upper
+  var upper = "";
+  var u1 = prompt("Would like to include upper-case letters", "yes or no");
+  if (u1 == "yes") {
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (u1 == "no") {
+    upper = "";
+  }
+  chars = lower + upper;
+  console.log("Character string is " + chars);
+
+  //Checks for numbers
+  var numbers = "";
+  var n1 = prompt("Would like to include numbers in your password", "yes or no");
+  if (n1 == "yes") {
+    numbers = "1234567890";
+  }
+  if (n1 == "no") {
+    numbers = "";
+  }
+  chars = lower + upper + numbers;
+  console.log("Character string is " + chars);
+
   //Checks if the user wants to add special characters
   var special = "";
-  var s1 = prompt ("Would like to include special characters in this lines?", "yes or no");
-  if (s1 == "yes"){
+  var s1 = prompt("Would like to include special characters in this password?","yes or no");
+  if (s1 == "yes") {
     special = "!@#$%^&*()";
   }
-  if (s1 == "no"){ 
+  if (s1 == "no") {
     special = "";
   }
-  chars = special;
+  chars = lower + upper + numbers + special;
   console.log("Character string is " + chars);
-
-
-
-
-
+  //
 }
 
 // Add event listener to generate button
