b698676b88e2d3fe7a3fe9b5b716f7034d3b7648
mikejmarin25 <105763252+mikejmarin25@users.noreply.github.com>
2022-06-28 23:33:36 -0400

    added password generator funtionality

--- Develop/script.js
+++ Develop/script.js
@@ -71,7 +71,15 @@ function writePassword() {
   }
   chars = lower + upper + numbers + special;
   console.log("Character string is " + chars);
-  //
+  
+
+  var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+  }
+        document.getElementById("password").value = password;
+ 
 }
 
 // Add event listener to generate button
